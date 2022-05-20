6cee05d743233d4cae70c2e0f6756b00c9b2ff37
lucas-mac <lucasmaia@dcc.ufmg.br>
2022-05-20 17:54:44 -0300

    jogador att

--- jogador.h
+++ jogador.h
@@ -2,14 +2,19 @@
 
 class Jogador {
  public:
-  void tira_saldo(int a);
-  void soma_saldo(int a);
-  int get_saldo(int a);
-  void get_name();
-  void set_name(std::string name);
+  Jogador(std::string nome, int saldo);
+  void tira_saldo(int a) { _saldo -= a; };
+  void soma_saldo(int a) { _saldo += a; };
+  int get_saldo() { return _saldo; };
+  std::string get_name() { return _nome; };
 
  private:
-  std::string nome;
-  int saldo;
-  carta mao[5];
+  std::string _nome;
+  int _saldo;
+  carta _mao[5];
 };
+
+Jogador::Jogador(std::string nome, int saldo) {
+  _nome = nome;
+  _saldo = saldo;
+}
